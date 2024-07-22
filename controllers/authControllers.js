7890ddd2a806f4cf37a05e99a0bfe71aad160e1d
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-07-22 18:23:38 +0530

    nothing working

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -9,9 +9,9 @@ const nodemailer = require("nodemailer");
 
 const cookieOptions = {
   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
-  // secure: process.env.NODE_ENV === "production", 
-  // httpOnly: true, 
-  // sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", 
+  secure: process.env.NODE_ENV === "production", 
+  httpOnly: true, 
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", 
 };
 
 const transporter = nodemailer.createTransport({
