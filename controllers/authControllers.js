86aa83b5c301affdc00d40f81b734db2ead45e89
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-07-22 18:02:35 +0530

    trying to fix cookie

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -10,7 +10,6 @@ const nodemailer = require("nodemailer");
 const cookieOptions = {
   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
   secure: process.env.NODE_ENV === "production", 
-  httpOnly: true, 
   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", 
 };
 
